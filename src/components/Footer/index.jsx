491f11d1144a13a8f212e826634830e4b7ebadb2
Beatriz Penalva <biapenalva@gmail.com>
2021-10-14 07:59:27 -0300

    chore: create a folder to footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <>
+    <footer>
+      © developed by
+      <a href='https://github.com/cbalieiro/SAP005-burger-queen'>
+        foodie! systems
+      </a>
+    </footer>
+  </>
+)
+
+export default Footer
