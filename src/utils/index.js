cc26a39b2fe6c519f61578389df1640b8815e809
Beatriz Penalva <biapenalva@gmail.com>
2021-10-14 07:58:26 -0300

    chore: delete function not in use

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,27 +1,20 @@
 export const calculateTotal = (productsChart) =>
   productsChart.reduce((accumulator, current) => {
-    const { quantity, price } = current
-    accumulator = Number(quantity * price + accumulator)
+    const { qtd, price } = current
+    accumulator = Number(qtd * price + accumulator)
     return accumulator
   }, 0)
 
 export const updateChartItem = (productToSet, productsChart) =>
   productsChart.map((i) => {
     if (i.id === productToSet.id) {
-      i.quantity += productToSet.quantity
+      i.qtd += productToSet.qtd
     }
     return i
   })
 
 export const isOnTheList = (productToSet, productsChart) =>
   productsChart.some((item) => item.id === productToSet.id)
-
-export const listItemsOrder = (products) => {
-  products.map((item) => ({
-    id: item.id,
-    qtd: item.quantity,
-  }))
-}
 
 export const getBurger = (burgerList, chosenBurger) => {
   burgerList.find((item) => {
