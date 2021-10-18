bcfc5a0b0405b75ce3fca37658faf9b0244205cf
Beatriz Penalva <biapenalva@gmail.com>
2021-10-18 05:45:04 -0300

    chore: remove old code

--- src/components/Main.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
-
-const Container = ({ children }) => (
-  <main className='body-default'>{children}</main>
-)
-
-export default Container
