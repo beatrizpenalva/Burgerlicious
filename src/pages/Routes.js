36a174bfa2ce164c8d76390b10b4a783678556cf
Beatriz Penalva <biapenalva@gmail.com>
2021-11-01 17:23:51 -0300

    chore: disable lint forbidden spread props rule

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react'
 import PropTypes from 'prop-types'
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
